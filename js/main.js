b6bf214777fc91fcb43ade2d9535ad01338a4771
thienan1404 <thienan1404@yahoo.com.vn>
2023-10-14 17:04:30 -0700

    page done

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var countDownDate = new Date("Oct 21, 2023 17:38:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="timer"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "FINAL STEPS TO TOTAL ENLIGHTENMENT:<br>14603 14th Ave SW, Burien, WA 98166 ";
+  }
+}, 1000);
