7dca938e9877087ddd3e7d2b429903c2c0970bb9
thienan1404 <thienan1404@yahoo.com.vn>
2023-10-21 16:33:40 -0700

    time change again

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Oct 21, 2023 16:33:00").getTime();
+var countDownDate = new Date("Oct 21, 2023 16:36:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
